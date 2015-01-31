7dea7f948e012f2cf927a53fd8670716f4d02e5f
Patrick Waivers <pwaivers@gmail.com>
2015-01-31 00:38:19 -0500

    none change

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,8 +9,8 @@
 
 module.exports = function (grunt) {
 
-  // Load grunt tasks automatically
-  require('load-grunt-tasks')(grunt);
+		// Load grunt tasks automatically
+		require('load-grunt-tasks')(grunt);
 
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
